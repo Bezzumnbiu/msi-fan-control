d630c4ee6c9f4d7355a9613608be1c6332a355d7
e3f <bezzumnbiu@gmail.com>
2023-01-04 01:32:59 +0500

    Another bug fix. Don't worry, there's only a 65% chance to destroy your laptop now

--- src/main.cpp
+++ src/main.cpp
@@ -6,28 +6,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-// For futures //
-
-//int readACBConfig() {
-//
-//    ifstream inFile;
-//    inFile.open(AUTOCOOLER_BOOST_CONFIG_FILE);
-//    std::vector<std::string> vector_config;
-//    std::string line;
-//    if (inFile.is_open()){
-//        while (getline(inFile, line, '"')){
-//            if(line.find('#') == 0) continue;
-//                vector_config.push_back(line);
-//        }
-//    }
-//    for (int i=20; i < vector_config.size(); i++) {
-//        std::cout << vector_config[i] << "-i";
-//    }
-//
-//    std::ifstream vector_config_file("AUTOCOOLER_BOOST_CONFIG_FILE");
-//
-//}
-
 void setFanTable() {
     std::fstream file;
     file.open(EC_FILE, std::ios::binary | std::ios::out | std::ios::in);
@@ -44,7 +22,7 @@ void setFanTable() {
         file.seekp(CPU_FAN_SPEED_2); file.put(0x28);
         file.seekp(CPU_FAN_SPEED_3); file.put(0x3B);
         file.seekp(CPU_FAN_SPEED_4); file.put(0x50);
-        file.seekp(CPU_FAN_SPEED_5); file.put(0x64);
+        file.seekp(CPU_FAN_SPEED_5); file.put(0xFF);
 
         file.seekp(GPU_TEMP_0); file.put(0x37);
         file.seekp(GPU_TEMP_1); file.put(0x41);
@@ -58,7 +36,7 @@ void setFanTable() {
         file.seekp(GPU_FAN_SPEED_2); file.put(0x28);
         file.seekp(GPU_FAN_SPEED_3); file.put(0x3B);
         file.seekp(GPU_FAN_SPEED_4); file.put(0x50);
-        file.seekp(GPU_FAN_SPEED_5); file.put(0x64);
+        file.seekp(GPU_FAN_SPEED_5); file.put(0xFF);
 
         file.seekp(BATTERY_CHARGING_THRESHOLD); file.put(0x5A);
         file.seekp(FAN_MODE); file.put(0x8C);
@@ -70,38 +48,42 @@ void setFanTable() {
 }
 
 int control_ACB() {
-
+    //Open EC file from debugfs and initialize realTemp array with values of... Real Temperatures?
     std::fstream file;
     file.open(EC_FILE, std::ios::binary | std::ios::out | std::ios::in);
     uint16_t realTemp[] = {0, 0};
 
     while (file.is_open()){
+        // Checks and write temperatures to realTemp buffer
         file.seekg(REALTIME_CPU_TEMP);
         file.read((char *) &realTemp[0], sizeof(uint16_t));
         file.seekp(REALTIME_GPU_TEMP);
         file.read((char *) &realTemp[1], sizeof(uint16_t));
-        if (realTemp[0] > CPU_MAX_TEMP || realTemp[1] > GPU_MAX_TEMP) {
-            file.seekp(COOLER_BOOST) ;
+        // Move to Cooler Boost address
+        file.seekp(COOLER_BOOST) ;
+        // Write value to start Cooler Boost
+        if ( bool((realTemp[0] > CPU_MAX_TEMP) || (realTemp[1] > GPU_MAX_TEMP)) ) {
             file.put(COOLER_BOOST_ON);
             }
-        if (realTemp[0] < (CPU_MAX_TEMP - 0x10) || realTemp[1] < (GPU_MAX_TEMP - 0x10)) {
-            file.seekp(COOLER_BOOST)  ;
+        if ( bool((realTemp[0] < (CPU_MAX_TEMP - 0x0A)) && ((realTemp[1] < (GPU_MAX_TEMP - 0x0A)))) ) {
             file.put(COOLER_BOOST_OFF);
         }
         file.close();
     }
-    usleep(500);
+    sleep(1);
     return 0;
 }
 
 
 int main(int argc, char *argv[])
 {
-// Need to support write to EC
+// Need to support write to EC, checks kernel modules if have errors
     system("modprobe ec_sys write_support=1");
 
 // readACBConfig();
+    // Write EC table to works cooler fine
     setFanTable();
+    //Enable infilite loop...
     while (true) {
         control_ACB();
     }
